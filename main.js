09fe45eec72974b5a93ced8c4f4b7a00162ffe5e
DAVID ALVES TRESANO <38036@americana.fatec.br>
2021-11-11 18:23:47 -0300

    a

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 function consulta(){
+    var cep = document.getElementById("cep").value;
+    var url = "https://viacep.com.br/ws/"+cep+"/json/";
+    console.log(cep);
     $.ajax({
-        url: "https://viacep.com.br/ws/14800390/json/",
+        url: url,
         type: "GET",
         success: function(response){
             console.log(response);
+            $("#logradouro").html(response.logradouro);
+            $("#bairro").html(response.bairro);
+            $("#complemento").html(response.complemento);
+            $("#uf").html(response.uf);
+            $("#localidade").html(response.localidade);
+            $("#siafi").html(response.siafi);
+            $("#ibge").html(response.ibge);
+            $("#gia").html(response.gia);
+            $("#ddd").html(response.ddd);
         }
     })
 }
